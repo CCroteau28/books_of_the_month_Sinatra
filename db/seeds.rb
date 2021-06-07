52509446d1fd35ffcd6de9c0ce0cee8182e3b165
CCroteau28 <ccroteau28@gmail.com>
2021-06-06 23:13:06 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,83 @@
 books_list = {
     "Instructions For Dancing" => { 
-    :genre => "Young Adult" 
+    :popular => "June" 
     }, 
     "Skye Falling" => {  
-    :genre => "Contempory Fiction" 
+    :popular => "June" 
     },
     "HALF SICK of SHADOWS" => {  
-    :genre => "Fantasy" 
+    :popular => "June" 
     },
     "The Maidens" => {  
-    :genre => "Thriller" 
+    :popular => "June" 
     },
     "Malibu Rising" => {  
-    :genre => "Historical Fiction" 
+    :popular => "June" 
+    },
+    "Things We Lost To The Water" => {
+    :popular => "May"
+    },
+    "How Lucky" => {
+    :popular => "May"
+    },
+    "The Last Thing He Told Me" => {
+    :popular => "May"
+    },
+    "Imposter Syndrome" => {
+    :popular => "May"
+    },
+    "Ariadne" => {
+    :popular => "May"
+    }
+}
+
+books_list.each do |name, book_hash|
+    b = Book.new
+    b.name = name
+    book_hash.each do |attribute, value|
+        b[attribute] = value
+    end
+    b.save
+end
+
+author_list = {
+    "Nicola Yoon" => {
+    },
+    "Mia Mckenzie" => {
+    },
+    "Laura Sebastian" => {
+    },
+    "Alex Michaelides" => {
+    },
+    "Taylor Jenkins Reid" => {
+    },
+    "Eric Nguyen" => {
+    },
+    "Will Leitch" => {
+    },
+    "Laura Dave" => {
+    },
+    "Kathy Wang" => {
+    },
+    "Jennifer Saint" => {
+    }
+}
+
+author_list.each do |name, author_hash|
+    b = Author.new
+    b.name = name
+    b.save
+end
+
+genre_list = {
+    "Young Adult" => {
+    },
+    "Contemporary Fiction" {
+    },
+    "Fantasy" => {
+    },
+    "Thriller" => {
+    },
+    "Historical Fiction" => {
     }
 }
